bbd536fea6a70d8f223047ef42ffa1ea1f9a527c
Khalid Masum <khalid.masum.92@gmail.com>
2026-08-07 14:28:03 +0600

    add persona config

---
+++ persona.config.js
@@ -0,0 +1,14 @@
+export default {
+    githubPages: {
+        url: "https://labnann.github.io",
+        baseUrl: "/",
+    },
+
+    title: "Persona",
+    logo: 
+    {
+          alt: "Persona Logo",
+          src: "img/logo.png"
+    },
+    copyright: `Copyright © ${new Date().getFullYear()} Persona, Inc. Built with Persona.`
+};
